4862896a1b9cb4d80781454c07fbb6c59c98bd79
Sarunas Valaskevicius <rakatan@gmail.com>
2014-09-27 17:17:46 +0100

    fix message priorities

--- scripts/battery_poller.sh
+++ scripts/battery_poller.sh
@@ -17,13 +17,13 @@ FULL=$(cat /sys/class/power_supply/BAT0/energy_full)
 
 PERCENT=$(expr $NOW \* 100 / $FULL)
 
-if test "$PERCENT" -lt 10 ; then
-    notify-send "Battery is getting LOW!" "Just $PERCENT % of the battery is left - hurry up!"
-fi
-
 if test "$PERCENT" -lt 5 ; then
-    notify-send -t 60000 "JUICE!!!" "Just $PERCENT % of the battery is left. I'm going to sleep." 
+    notify-send -t 60000 "JUICE!!!" "Just $PERCENT% of the battery is left. I'm going to sleep." 
     sleep 10
     /usr/sbin/pm-suspend
+else
+    if test "$PERCENT" -lt 10 ; then
+        notify-send "Battery is getting LOW!" "Just $PERCENT% of the battery is left - hurry up!"
+    fi
 fi
 
